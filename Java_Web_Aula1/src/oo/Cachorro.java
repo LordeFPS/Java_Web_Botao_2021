fea8f453c0a900714c8b71e356f107073e89b8ed
lordefps <lodizinho_mice@hotmail.com>
2021-04-03 14:23:17 -0300

    Alterando Cachorro

--- Java_Web_Aula1/src/oo/Cachorro.java
+++ Java_Web_Aula1/src/oo/Cachorro.java
@@ -2,8 +2,8 @@ package oo;
 
 public class Cachorro extends Animal {
 	
-	public Cachorro(String nome, String especie) {
-		super(nome, especie, 0);
+	public Cachorro(String nome) {
+		super(nome, "Cachorro");
 	}
 	
 	public Cachorro(String nome, String especie, int idade) {
