2e5025953bdb0f007cd80c2784ea5737d8fc588a
lordefps <lodizinho_mice@hotmail.com>
2021-04-03 14:36:33 -0300

    Aula 2 Teste Classe Gato

--- Java_Web/src/oo/TesteAnimal.java
+++ Java_Web/src/oo/TesteAnimal.java
@@ -3,10 +3,11 @@ package oo;
 public class TesteAnimal {
 
 	public static void main(String[] args) {
+		/*
 		// metodo construtor, o new ele inicializa o nosso objeto
 		Cachorro n1 = new Cachorro("spike");
 		
-		System.out.println("O nome do animal e " + n1.getNome() + 
+		System.out.println("O nome do animal se chama " + n1.getNome() + 
 						   " e pertence a especie " + n1.getEspecie()+
 						   " e tem idade de " + n1.getIdade());
 		if (n1.isAdulto() == true) {
@@ -20,7 +21,7 @@ public class TesteAnimal {
 		Cachorro n2 = new Cachorro("Churras", "Xauxau", 2);
 		
 		// tudo que for obrigatório na classe é preciso colocar no constutor
-		System.out.println("O nome do animal e " + n2.getNome() + 
+		System.out.println("O nome do animal se chama " + n2.getNome() + 
 				   		   " e pertence a especie " + n2.getEspecie() +
 				   		   " e tem idade de " + n2.getIdade());
 		if (n2.isAdulto()) {
@@ -29,14 +30,18 @@ public class TesteAnimal {
 			System.out.println("Animal filhote.");
 		}
 		n2.emitirSom();
+		*/
 		
-		// Cadastrando um gato
-		
+		// Cadastrando um gato Aula 2 
 		Gato gt1 = new Gato("Lorde");
-		System.out.println("O nome do animal e " + gt1.getNome() + 
-				   " e pertence a especie " + gt1.getEspecie()+
+		System.out.println("O nome do animal se chama " + gt1.getNome() + 
+				   " e pertence a especie " + gt1.getEspecie() +
 				   " e tem idade de " + gt1.getIdade());
-		
+		if (gt1.isAdulto()) {
+			System.out.println(gt1.getEspecie() + " adulto.");
+		}else {
+			System.out.println(gt1.getEspecie() + " filhote.");
+		}
 		
 
 	}
