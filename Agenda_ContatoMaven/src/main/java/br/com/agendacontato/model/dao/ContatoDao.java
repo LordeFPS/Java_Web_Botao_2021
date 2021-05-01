cbb00404dcba628c21535d074e190b5cf7bb0438
lordefps <lodizinho_mice@hotmail.com>
2021-05-01 16:40:25 -0300

    Aula 6 daoContato metodo delete

--- Agenda_ContatoMaven/src/main/java/br/com/agendacontato/model/dao/ContatoDao.java
+++ Agenda_ContatoMaven/src/main/java/br/com/agendacontato/model/dao/ContatoDao.java
@@ -80,9 +80,16 @@ public class ContatoDao implements BaseDao<Contato, Long>{
 		
 	}
 
-	public void delete(Long id) {
-		// TODO Auto-generated method stub
-		
+	public void delete(Long id) throws SQLException {
+
+		statement = "DELETE FROM contato WHERE id = ?";
+	    	try (PreparedStatement preparedStatement = this.conexao.getConnection().prepareStatement(statement)) {
+	            preparedStatement.setLong(1, id);
+	            this.conexao.commit();
+	        } catch (SQLException e) {
+	            this.conexao.rollback();
+	            throw e;
+	        }
 	}
 	
 }
